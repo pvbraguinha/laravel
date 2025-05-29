f5db0590e78e5942510fb5ad1ab891a3d5245512
pvbraguinha <pvbraguinha@gmail.com>
2025-05-29 15:19:35 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json([
-        'message' => 'API SmartDog rodando com sucesso!'
-    ]);
+    return 'Smartdog API está online!';
 });
